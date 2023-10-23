dd065de809e6db421cbe259e41ba9cd0d0cf732b
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-23 14:50:58 -0500

    Attached HTML elements into the new element

--- CustomComponents/my-element.js
+++ CustomComponents/my-element.js
@@ -1,9 +1,11 @@
 class MyElement extends HTMLElement{
   constructor(){
     super();
-    console.log('Hello world');
+    this.p = document.createElement('p');
   }
-  
+  connectedCallback(){
+    this.p.textContent = 'Hello world';
+    this.appendChild(this.p);
 }
 
 customElements.define('my-element', MyElement);
