161649099ca1c936216a7d999eb1519b8872bc8e
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-23 15:09:59 -0500

    Tested disconnectedCallback and attributeChangedCallback  cycles.

--- CustomComponents/my-element.js
+++ CustomComponents/my-element.js
@@ -1,11 +1,43 @@
+
+
+const template = document.createElement('div');
+template.innerHTML = `
+  <style>
+    .text1{
+      color: red;
+    }
+    p{
+      color: blue;
+
+    }
+  </style>
+  <p class="text1"> Hello world </p>
+  <p> from template </p>
+`;
+
 class MyElement extends HTMLElement{
+  static observedAttributes = ["size"];
   constructor(){
     super();
     this.p = document.createElement('p');
   }
   connectedCallback(){
+    this.setAttribute('size','200');
     this.p.textContent = 'Hello world';
     this.appendChild(this.p);
+    this.appendChild(template);
+
+    console.log('Connected');
+  }
+
+  disconnectedCallback(){
+    console.log('Disconnected');
+  }
+
+  attributeChangedCallback(name,oldValue,newValue){
+    console.log(`Attribute ${name} changed from ${oldValue} to ${newValue}`);
+  }
+
 }
 
 customElements.define('my-element', MyElement);
