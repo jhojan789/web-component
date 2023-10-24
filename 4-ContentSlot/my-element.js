01d6e2aa3728accfc96482cb145e8fa01793afe8
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-24 10:04:08 -0500

    Added text dynamically from light DOM into the shadow DOM.

---
+++ 4-ContentSlot/my-element.js
@@ -0,0 +1,56 @@
+class MyElement extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode:'open'});
+  }
+
+  getTemplate(){
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <section>
+      <h2>
+        <slot></slot>
+      </h2>
+      <div>
+        <p>
+          <slot></slot>
+        </p>
+      
+      </div>
+    
+    
+    
+    </section>
+    ${this.getStyles()}
+    `;
+
+    return template;
+  }
+
+  getStyles(){
+    return `
+    <style>
+      h2 {
+        color: red;
+      }
+      p {
+        color: green;
+      }
+    
+    </style>
+    
+    
+    `;
+  }
+
+  render(){
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+
+  connectedCallback(){
+    this.render();
+  }
+
+}
+
+customElements.define('my-element', MyElement);
