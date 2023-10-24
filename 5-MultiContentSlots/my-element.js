1931f392cf02d12e681c49265e1b83b6be064c98
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-24 10:47:02 -0500

    Created multi content slots

---
+++ 5-MultiContentSlots/my-element.js
@@ -0,0 +1,51 @@
+class MyElement extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode:'open'});
+  }
+  getTemplate(){
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <section>
+      <h2>
+        <slot name="title"></slot>
+      </h2>
+      <div>
+        <p>
+          <slot name="paragraph"></slot>
+        </p>
+      </div>
+    
+    </section>
+    ${this.getStyles()}
+    `;
+    return template;
+  }
+  getStyles(){
+    return `
+    <style>
+      h2{
+        color: red;
+      }
+
+      p{
+        color: green;
+      }
+    </style>
+    
+    `;
+  }
+
+  render(){
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+
+  connectedCallback(){
+    this.render();
+  }
+
+
+
+}
+
+customElements.define('my-element',MyElement);
